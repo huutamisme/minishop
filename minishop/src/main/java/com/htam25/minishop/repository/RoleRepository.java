dafca58da15137b1a146a4273287ce108aea8d0f
huutamisme <hohuutam287@gmail.com>
2026-04-01 16:43:28 +0700

    Implement repository layer

---
+++ minishop/src/main/java/com/htam25/minishop/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.htam25.minishop.repository;
+
+import com.htam25.minishop.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
